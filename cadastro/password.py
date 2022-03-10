44b5f7c77991c90ba177fe92f1a82c613ce124c4
4l3xd <alessandra.olea@gmail.com>
2022-03-10 17:33:29 -0300

    otimizaçao da criaçao de IDs, sem repetiçao de Identificadores UNICOS

--- cadastro/password.py
+++ cadastro/password.py
@@ -1,27 +1,48 @@
-from approved_pass import Approved
-from caesar_cipher import Caesar
+import re
+import getpass #modulo que esconde a senha
 
-class Password():
-  def __init__(self):
-    pass
+class Approved():
+    def __init__(self):
+      pass
   
-  def cadastro(self):
-    self.senha = Approved().test_password()
-    
-    while self.senha == None:
-      self.senha = Approved().test_password()
+    def test_password(self):
+        print("A senha deve ter mais de 8 digitos, e alem disso conter, no minimo, um caractere especial, uma letra minuscula, uma letra maiuscula e um numero.")
+        password = getpass.getpass('Digite tua senha: ')
+  
+        minimal_number = 1
+        minimal_upper_char = 1
+        minimal_lower_char = 1
+        minimal_special_char = 1
+        minimal_len_char = 8
+
+        tam, up, low, num, esp = False, False, False, False, False
       
-      # Reverse Cipher
-      # http://inventwithpython.com/hacking (BSD Licensed)
+        if len(password or ()) < minimal_len_char:
+            print('Senha tem que ter no mínimo '+str(minimal_len_char)+' caracteres')
+        else:
+          tam = True
+      
+        if len(re.findall(r"[A-Z]", password)) < minimal_upper_char:
+            print('Senha tem que ter no mínimo '+str(minimal_upper_char)+' letras maiusculas')
+        else:
+          up = True 
+        
+        if len(re.findall(r"[a-z]", password)) < minimal_lower_char:
+            print('Senha tem que ter no mínimo '+str(minimal_lower_char)+' letras minusculas')
+        else:
+          low = True
+          
+        if len(re.findall(r"[0-9]", password)) < minimal_number:
+            print('Senha tem que ter no mínimo '+str(minimal_number)+' numeros')
+        else:
+          num = True
+          
+        if len(re.findall(r"[~`!@#$%^&*()_+=-{};:'><]", password)) < minimal_special_char:
+            print('Senha tem que ter no mínimo '+str(minimal_special_char)+' caracteres especiais')
+        else:
+          esp = True
 
-    #translated = ''
-  
-    #i = len(self.senha) - 1
-
-    #while i >= 0:
-    #    translated = translated + self.senha[i]
-    #    i = i - 1 #decrementação/subtração
-
-    self.senha = Caesar(self.senha).cripto()
-  
-    return self.senha
+        if esp == True and num == True and low == True and up == True and tam == True:
+          print("Ta-dã! senha valida!")
+          return password
+          
--- cadastro/user.py
+++ cadastro/user.py
@@ -7,7 +7,18 @@ class User():
     pass
     
   def cadastro(self):
-    self.id = random.randint(1000, 9999) # TO DO: pode duplicar valores, tratar!
+
+    self.id = random.randint(1000, 9999)
+    
+    with open('banco_dados.json', 'r') as bd:
+      dados = json.load(bd)
+      while self.id in dados:
+        self.id = random.randint(1000, 9999)
+        #print(cod)
+        #print("ID já existe!")
+        #print("{}".format(self.id))
+        #print(f"{self.id}")
+      
     self.nome = input("Digite seu nome: ")
     self.idade = input("Digite tua idade: ")
     self.nacionalidade = input("Digite sua nacionalidade: ")
@@ -20,6 +31,7 @@ class User():
     self.banco_dados[self.id]['nacionalidade'] = self.nacionalidade.strip()
     self.banco_dados[self.id]['senha'] = self.senha
     
-    with open('banco_dados.json', 'w') as json_file:
-      json.dump(self.banco_dados, json_file, indent=3, ensure_ascii=False)
+    with open('banco_dados.json', 'r+') as json_file:
+      dados.update(self.banco_dados)
+      json.dump(dados, json_file, indent=3, ensure_ascii=False)
     return print("Seu cadastro foi finalizado!")
